b1a79833f61bd7eaa2f23be3e6ef5204c322bae3
Ben Dreyer <bendreyer21@gmail.com>
2023-05-02 10:22:22 -0600

    Initial Commit

---
+++ Radiant/RadiantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadiantApp.swift
+//  Radiant
+//
+//  Created by Ben Dreyer on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RadiantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
