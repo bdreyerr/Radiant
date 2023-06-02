c6ad4fcc3125834987c07a4799b596dba82b3e88
bdreyerr <bendreyer21@gmail.com>
2023-06-02 11:56:40 -0600

    Sync the UI messages to the messages for a user stored in Firestore. Also add a clear messages button, wipes out all a user's messages from Firestore.

--- Radiant/Controllers/ChatManager.swift
+++ Radiant/Controllers/ChatManager.swift
@@ -6,8 +6,13 @@
 //
 
 import Foundation
+import AuthenticationServices
+import CryptoKit
+import Foundation
 import FirebaseCore
+import FirebaseAuth
 import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 
 class ChatManager: ObservableObject {
@@ -55,7 +60,52 @@ class ChatManager: ObservableObject {
     }
     
     // TODO: Fill in and move into the view to load the messages into a list
-    func retrieveMessages() {
+    func retrieveMessages(userID: String) {
         
+        var messages: [Message] = []
+        
+        let collectionRef = self.db.collection(Constants.FStore.messageCollectionName).whereField("userID", isEqualTo: userID).order(by: "date", descending: false)
+        
+        collectionRef.getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error retrieving comments: \(err.localizedDescription)")
+            } else if let querySnapshot = querySnapshot {
+                for document in querySnapshot.documents {
+                    let message = Message(
+                        id: document.documentID,
+                        userID: document.data()["userID"] as? String,
+                        isMessageFromUser: document.data()["isMessageFromUser"] as? Bool,
+                        content: document.data()["content"] as? String,
+                        date: document.data()["date"] as? Date)
+                    messages.append(message)
+                    print("Message was retrieved, messageID: \(document.documentID), message content: \(document.data()["content"] as? String ?? "No Content")")
+                }
+            }
+        }
     }
+    
+    func clearMessages(userID: String) {
+        
+        // lookup and delete all messages where the userID = userID
+        let collectionRef = self.db.collection(Constants.FStore.messageCollectionName)
+        let query = collectionRef.whereField("userID", isEqualTo: userID)
+        
+        query.getDocuments() { (snapshot, error) in
+            if let err = error {
+                print("error getting messages to delete: \(err.localizedDescription)")
+                return
+            }
+                
+            for document in snapshot!.documents {
+                document.reference.delete() { error in
+                    if let e = error {
+                        print("error deleting document: \(e.localizedDescription)")
+                    } else {
+                        print("Document deleted!")
+                    }
+                }
+            }
+        }
+    }
+    
 }
--- Radiant/Views/ForumViews/RadiantBotChatView.swift
+++ Radiant/Views/ForumViews/RadiantBotChatView.swift
@@ -6,6 +6,10 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 struct RadiantBotChatView: View {
     
@@ -15,13 +19,37 @@ struct RadiantBotChatView: View {
     @EnvironmentObject var profileStateManager: ProfileStatusManager
     @StateObject var chatManager = ChatManager()
     
+    let db = Firestore.firestore()
+    
+    @State var messages: [Message] = []
+    
     var body: some View {
         ZStack {
             
             VStack {
                 
-                Text("Radiant Chat")
-                    .font(.system(size: 24))
+                HStack {
+                    Text("Radiant Chat")
+                        .font(.system(size: 24))
+                    
+                }
+                
+                Button(action: {
+                    self.messages = []
+                    if let user = profileStateManager.userProfile {
+                        chatManager.clearMessages(userID: user.id!)
+                    }
+                }) {
+                    Image(systemName: "arrow.clockwise")
+                        .resizable()
+                        .frame(maxWidth: 20, maxHeight: 20, alignment: .trailing)
+                        .padding(.leading, 250)
+                        .foregroundColor(.white)
+                }
+                
+                
+                
+                
                 
                 
                 // This is the scroll view.
@@ -30,37 +58,39 @@ struct RadiantBotChatView: View {
                     // Messages
                     VStack(alignment: .leading) {
                         
-                        // Message from you
-                        MessageFromYou(text: "Hi Radiant Bot, you're so cool!")
+                        ForEach(messages) { message in
+                            if message.id != nil {
+                                if (message.isMessageFromUser!) {
+                                    MessageFromYou(text: message.content)
+                                } else {
+                                    MessageFromBot(text: message.content)
+                                }
+                            } else {
+                                Text("Messages would be here!")
+                            }
+                        }
                         
-                        
-                        // Message from Radiant Bot
-                        MessageFromBot(text: "You know it G")
-                        
-                        
-                        MessageFromYou(text: "Can you help me with my issues?")
-                        
-                        MessageFromBot(text: "Of course homie")
                     }
                     
                 }.padding(.top, 20)
                 
                 // Message send bar
-                
                 HStack {
                     TextField("Enter text", text: $text)
                         .padding()
                         .background(RoundedRectangle(cornerRadius: 40).foregroundColor(.white)).frame(maxWidth: 360, maxHeight: 40)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 40).stroke(Color.black, lineWidth: 2).frame(width: 360, height: 40)
-                        )
+//                        .overlay(
+//                            RoundedRectangle(cornerRadius: 40).stroke(Color.black, lineWidth: 2).frame(width: 360, height: 40)
+//                        )
                         .foregroundColor(.black)
 //                        .padding(.bottom, 100)
+                    
                         .padding(.leading, 70)
                     
                     Button(action: {
                         if let user = profileStateManager.userProfile {
                             chatManager.sendMessage(userID: user.id!, content: self.text)
+                            self.retrieveMessages(userID: user.id!)
                         }
                         self.text = ""
                     }) {
@@ -77,7 +107,37 @@ struct RadiantBotChatView: View {
         }
         .background(Color.clear)
         .environmentObject(chatManager)
+        .onAppear {
+            if let user = profileStateManager.userProfile {
+                retrieveMessages(userID: user.id!)
+            }
+        }
     }
+    
+    
+    func retrieveMessages(userID: String) {
+        self.messages = []
+        
+        let collectionRef = self.db.collection(Constants.FStore.messageCollectionName).whereField("userID", isEqualTo: userID).order(by: "date", descending: false)
+        
+        collectionRef.getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error retrieving comments: \(err.localizedDescription)")
+            } else if let querySnapshot = querySnapshot {
+                for document in querySnapshot.documents {
+                    let message = Message(
+                        id: document.documentID,
+                        userID: document.data()["userID"] as? String,
+                        isMessageFromUser: document.data()["isMessageFromUser"] as? Bool,
+                        content: document.data()["content"] as? String,
+                        date: document.data()["date"] as? Date)
+                    self.messages.append(message)
+                    print("Message was retrieved, messageID: \(document.documentID), message content: \(document.data()["content"] as? String ?? "No Content")")
+                }
+            }
+        }
+    }
+    
 }
 
 struct RadiantBotChatView_Previews: PreviewProvider {
